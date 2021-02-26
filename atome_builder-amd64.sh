860da691c69594f74852f7da0d394879cc1af77b
Romain Tartière <romain@blogreen.org>
2021-02-26 07:39:35 -1000

    Finish amd64 integration

---
+++ atome_builder-amd64.sh
@@ -0,0 +1,9 @@
+pw adduser firefox -m
+pkg install firefox fvwm slim xorg-minimal
+
+sysrc dbus_enable=yes
+sysrc slim_enable=yes
+
+sed -i '' \
+	-e 's/^#default_user.*/default_user firefox/' \
+	-e 's/^#auto_login/auto_login yes/'
