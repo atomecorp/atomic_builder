d2f2182022386d2a2ae88b4941489a99de1ccb6a
Romain Tartière <romain@blogreen.org>
2021-03-19 07:20:35 -1000

    Add chormium support

--- atome_builder-amd64.sh
+++ atome_builder-amd64.sh
@@ -1,10 +1,11 @@
+pw adduser chromium -m
 pw adduser firefox -m
-pkg install -y firefox fvwm slim xorg-minimal
+pkg install -y chromium firefox fvwm slim xorg-minimal
 
 sysrc dbus_enable=yes
 sysrc slim_enable=yes
 
 sed -i '' \
-	-e 's/^#default_user.*/default_user firefox/' \
+	-e 's/^#default_user.*/default_user chromium/' \
 	-e 's/^#auto_login.*/auto_login yes/' \
 	/usr/local/etc/slim.conf
